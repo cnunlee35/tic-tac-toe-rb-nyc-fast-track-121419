daee21728da908aa3ad22af11b8e16ba5d75d4a6
celebrated-ninja-3247 <cnunlee7013@gmail.com>
2019-12-15 19:17:07 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,7 +1,24 @@
 WIN_COMBINATIONS = [
   [0,1,2],
   [3,4,5],
-  [6,7,8]
+  [6,7,8],
+  [0,4,8],
+  [2,4,6],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8]
   ]
   board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-  
+  def won?(board_array)
+    WIN_COMBINATIONS.each do |combo_arr|
+      first_index = combo_arr[0]
+      second_index = combo_arr[1]
+      third_index = combo_arr[2]
+      
+      symbol_1 = board_array[first_index]
+      symbol_2 = board_array[second_index]
+      symbol_3 = board_array[third_index]
+      if symbol_1 == symbol_2 && symbol_2 == symbol_3
+      end
+    end
+  end
